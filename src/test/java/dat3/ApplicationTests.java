56c5ad7bef0b117acc0a3f38f19d885048442054
JohannesSloth <joha218s@stud.kea.dk>
2023-01-06 13:36:26 +0100

    retention-days added to actions-yml file. ApplicationTest outcommented.

--- src/test/java/dat3/ApplicationTests.java
+++ src/test/java/dat3/ApplicationTests.java
@@ -3,11 +3,11 @@ package dat3;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+/*@SpringBootTest
 class ApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
-}
+}*/
